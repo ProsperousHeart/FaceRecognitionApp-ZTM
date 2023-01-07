2f41d278240877a58b771c1375db22b7b2814c79
Kassandra Keeton (the Prosperous Heart) <ProsperousHeart@users.noreply.github.com>
2023-01-07 15:01:43 -0600

    include component in App

--- facerecognitionapp-ztm/src/App.js
+++ facerecognitionapp-ztm/src/App.js
@@ -1,15 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
+import FunBG from './components/FunBG/FunBG';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import Rank from './components/Rank/Rank';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 
 // function App() { // original - functional component
+
 class App extends Component {
+
   render() {
     return (
       <div className="App">
+        <FunBG id="tsparticles" />
+        {/*<Particles options={particlesOptions}/>*/}
         <Navigation />
         <Logo />
         <Rank />
