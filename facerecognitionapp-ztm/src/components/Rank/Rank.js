a328c5b1ca9e564e34d10fcb9607fb4ec161f119
Kassandra Keeton (the Prosperous Heart) <ProsperousHeart@users.noreply.github.com>
2023-01-07 00:13:39 -0600

    importing CSS

--- facerecognitionapp-ztm/src/components/Rank/Rank.js
+++ facerecognitionapp-ztm/src/components/Rank/Rank.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import './Rank.css';
 
 const Rank = () => {
     return (
         <div>
-
+            <div className='white f3 rank-name'>
+                {'CREATOR, your current rank is ...'}
+            </div>
+            <div className='white f1 rank'>
+                {'#42'}
+            </div>
         </div>
     )
 }
