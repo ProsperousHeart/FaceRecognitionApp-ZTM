64e9c4be832606d7a729da7c129d1be84c05b45c
Kassandra Keeton (the Prosperous Heart) <ProsperousHeart@users.noreply.github.com>
2023-01-06 21:56:15 -0600

    migration from functino to class

--- facerecognitionapp-ztm/src/App.js
+++ facerecognitionapp-ztm/src/App.js
@@ -1,15 +1,18 @@
+import React, { Component } from 'react';
 import './App.css';
 import Navigation from './components/Navigation/Navigation';
 
-function App() {
-  return (
-    <div className="App">
-      <Navigation />
-      <Logo />
-      {/*<ImageLinkForm />
-      <FaceRecognition />*/}
-    </div>
-  );
+// function App() { // original - functional component
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Navigation />
+        {/*<Logo />
+        <ImageLinkForm />*/}
+      </div>
+    );
+  }
 }
 
 export default App;
