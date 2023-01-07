b5a1ba199d55b8a5a481b91fd7457d5f524b05e0
Kassandra Keeton (the Prosperous Heart) <ProsperousHeart@users.noreply.github.com>
2023-01-06 22:36:21 -0600

    add Logo component

--- facerecognitionapp-ztm/src/App.js
+++ facerecognitionapp-ztm/src/App.js
@@ -9,8 +9,8 @@ class App extends Component {
     return (
       <div className="App">
         <Navigation />
-        {/*<Logo />
-        <ImageLinkForm />*/}
+        <Logo />
+        {/*<ImageLinkForm />*/}
       </div>
     );
   }
