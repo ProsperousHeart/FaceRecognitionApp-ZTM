8dacf27c3d5e0fc34d05f4e344a4b682d8a55b38
Kassandra Keeton (the Prosperous Heart) <ProsperousHeart@users.noreply.github.com>
2023-01-06 22:37:31 -0600

    initial part of logo

--- facerecognitionapp-ztm/src/components/Logo/Logo.js
+++ facerecognitionapp-ztm/src/components/Logo/Logo.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import Tilt from 'react-parallax-tilt';
+import './Logo.css';
 
 const Logo = () => {
     return (
-        
+        <div className='ma4 mt0'>
+            <Tilt className='Tilt br2 shadow-2' perspective={500} glareEnable={true} 
+            glareMaxOpacity={0.45} scale={1.02} style={{ height: 150, width: 150 }}>
+                <div>
+                    <h1>React Parallax Tilt 👀</h1>
+                </div>
+            </Tilt>
+        </div>
     )
 }
 
