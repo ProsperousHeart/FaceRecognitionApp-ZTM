d78e8b210129adeb9e4073007de2cb351a8d0ab9
Kassandra Keeton (the Prosperous Heart) <ProsperousHeart@users.noreply.github.com>
2023-01-07 19:19:59 -0600

    integrate input change interaction

--- facerecognitionapp-ztm/src/components/ImageLinkForm/ImageLinkForm.js
+++ facerecognitionapp-ztm/src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './ImageLinkForm.css';
 
-const ImageLinkForm = () => {
+const ImageLinkForm = ({ onInputChange }) => {
     return (
         <div>
             <p className='f3'>
@@ -9,7 +9,8 @@ const ImageLinkForm = () => {
             </p>
             <div className='center'>
                 <div className='center form1 pa4 br3 shadow-5'>
-                    <input className='f4 pa2 w-70 center' type="text" /> {/* for URL of IMG */}
+                    {/*<input className='f4 pa2 w-70 center' type="text" /> /* for URL of IMG */}
+                    <input className='f4 pa2 w-70 center' type="tex" onChange={onInputChange} />
                     <button className='w-30 grow f4 link ph3 pv dib white bg-light-purple'>Detect</button>
                 </div>
             </div>
