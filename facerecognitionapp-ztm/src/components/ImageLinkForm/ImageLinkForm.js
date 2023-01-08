4b9c764d9af0805de05ddbe966ac8d1d38f71c83
Kassandra Keeton (the Prosperous Heart) <ProsperousHeart@users.noreply.github.com>
2023-01-07 19:40:33 -0600

    adding functionality to button click (submit)

--- facerecognitionapp-ztm/src/components/ImageLinkForm/ImageLinkForm.js
+++ facerecognitionapp-ztm/src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './ImageLinkForm.css';
 
-const ImageLinkForm = ({ onInputChange }) => {
+const ImageLinkForm = ({ onInputChange, onBtnSubmit }) => {
     return (
         <div>
             <p className='f3'>
@@ -11,7 +11,10 @@ const ImageLinkForm = ({ onInputChange }) => {
                 <div className='center form1 pa4 br3 shadow-5'>
                     {/*<input className='f4 pa2 w-70 center' type="text" /> /* for URL of IMG */}
                     <input className='f4 pa2 w-70 center' type="tex" onChange={onInputChange} />
-                    <button className='w-30 grow f4 link ph3 pv dib white bg-light-purple'>Detect</button>
+                    <button 
+                        className='w-30 grow f4 link ph3 pv dib white bg-light-purple'
+                        onClick={onBtnSubmit}
+                    >Detect</button>
                 </div>
             </div>
         </div>
