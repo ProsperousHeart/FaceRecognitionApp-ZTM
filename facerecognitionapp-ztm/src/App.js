cd05abb91abe4a620da7f7b07b44a9ba0c7dc91e
Kassandra Keeton (the Prosperous Heart) <ProsperousHeart@users.noreply.github.com>
2023-01-07 19:19:23 -0600

    prep for input interactivity - getting the text input

--- facerecognitionapp-ztm/src/App.js
+++ facerecognitionapp-ztm/src/App.js
@@ -9,6 +9,17 @@ import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 // function App() { // original - functional component
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+    }
+  }
+
+  onInputChange = (event) => {
+    //console.log(event);
+    console.log(event.target.value);
+  }
 
   render() {
     return (
@@ -18,7 +29,7 @@ class App extends Component {
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm />
+        <ImageLinkForm onInputChange={this.onInputChange} />
         {/*<FaceRecognition />*/}
       </div>
     );
