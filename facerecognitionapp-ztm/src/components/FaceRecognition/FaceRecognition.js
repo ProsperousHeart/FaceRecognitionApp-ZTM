58cb382b5f836c48fadda1b7f08e00abec124cc6
Kassandra Keeton (the Prosperous Heart) <ProsperousHeart@users.noreply.github.com>
2023-01-10 14:15:52 -0600

    add link to info

--- facerecognitionapp-ztm/src/components/FaceRecognition/FaceRecognition.js
+++ facerecognitionapp-ztm/src/components/FaceRecognition/FaceRecognition.js
@@ -5,7 +5,7 @@ import './FaceRecognition.css';
 
 const FaceRecognition = ({ boxes, imgURL }) => {
     //console.log({imgURL})
-    let img2show;
+    let img2show;  // https://reactjs.org/docs/conditional-rendering.html
     if (imgURL !== '') {
         {/*<img id="intputIMG" src={imgURL} alt="User Provided For Checking Faces" width='42%' height='auto' />*/}
         img2show = <img id="intputIMG" src={imgURL} alt="User Provided For Checking Faces" width='500px' height='auto' />;
