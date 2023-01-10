30c4e854c015dc6b3647757481b6253731ec7ee2
Kassandra Keeton (the Prosperous Heart) <ProsperousHeart@users.noreply.github.com>
2023-01-10 16:49:39 -0600

    update Nav based on route and if 'signed in'

--- facerecognitionapp-ztm/src/components/Navigation/Navigation.js
+++ facerecognitionapp-ztm/src/components/Navigation/Navigation.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import './Navigation.css';
 
-const Navigation = () => {
-    return (
-        <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
-            <p className='f3 link dim black underline pa3 pointer'>Sign Out</p>
-        </nav>
-    );
+const Navigation = ({ onRouteChg, isSignedIn }) => {
+    
+    if (isSignedIn) {
+        return (
+            <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
+                <p onClick={() => onRouteChg('signout')} className='f3 link dim black underline pa3 pointer'>Sign Out</p>
+            </nav>
+        );
+    } else {
+        return (
+            <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
+                <p onClick={() => onRouteChg('signin')} className='f3 link dim black underline pa3 pointer'>Sign In</p>
+                <p onClick={() => onRouteChg('register')} className='f3 link dim black underline pa3 pointer'>Register</p>
+            </nav>
+        );
+    }
 }
 
 export default Navigation;
