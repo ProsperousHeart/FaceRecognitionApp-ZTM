fa4260a252049b3aee32cf9d0acb70c7189eefff
Kassandra Keeton (the Prosperous Heart) <ProsperousHeart@users.noreply.github.com>
2023-01-10 11:31:50 -0600

    surrounding IMG with padding & restricting width

--- facerecognitionapp-ztm/src/components/FaceRecognition/FaceRecognition.js
+++ facerecognitionapp-ztm/src/components/FaceRecognition/FaceRecognition.js
@@ -1,12 +1,15 @@
 import React from 'react';
 //import './Navigation.css';
+//https://www.gannett-cdn.com/presto/2020/07/02/USAT/ff76bc68-557c-4af1-baaa-743fbeaa2d0d-AP_Film_-_Justice_League.jpg
 
 const FaceRecognition = ({ imgURL }) => {
     //console.log({imgURL})
     return (
         <div className='center ma'>
-            <img src={imgURL} alt="User Provided For Checking Faces" height='auto' />
-            {/*<img src="https://resume.prosperousheart.com/IMGs/20180108.jpg" alt="User Provided" />*/}
+            <div className='absolute mt2'>
+                <img src={imgURL} alt="User Provided For Checking Faces" width='42%' height='auto' />
+                {/*<img src="https://resume.prosperousheart.com/IMGs/20180108.jpg" alt="User Provided" />*/}
+            </div>
         </div>
     );
 }
