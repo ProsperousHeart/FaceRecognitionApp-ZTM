8d5aaf6d1a5fdd20dd774c626e03be82be0039cf
Kassandra Keeton (the Prosperous Heart) <ProsperousHeart@users.noreply.github.com>
2023-01-12 20:26:27 -0600

    making data dynamic

--- facerecognitionapp-ztm/src/components/Rank/Rank.js
+++ facerecognitionapp-ztm/src/components/Rank/Rank.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import './Rank.css';
 
-const Rank = () => {
+const Rank = ({name, entries}) => {
     return (
         <div>
             <div className='white f3 rank-name'>
-                {'CREATOR, your current rank is ...'}
+                {/*{'CREATOR, your current rank is ...'}*/}
+                {`${name}, your current rank ...`}
             </div>
             <div className='white f1 rank'>
-                {'#42'}
+                {/*{'#42'}*/}
+                {entries}
             </div>
         </div>
     );
