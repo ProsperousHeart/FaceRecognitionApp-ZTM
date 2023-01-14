81038beaf23068ce30916d9a86cd523f585cf4c4
Kassandra Keeton (the Prosperous Heart) <ProsperousHeart@users.noreply.github.com>
2023-01-14 14:56:16 -0600

    add error handling to fetch

--- facerecognitionapp-ztm/src/App.js
+++ facerecognitionapp-ztm/src/App.js
@@ -163,6 +163,7 @@ class App extends Component {
                 })*/
                 this.setState(Object.assign(this.state.user, { entries: count}));
               })
+              .catch(console.log)
           }
           this.dispFaceBox(this.calcFaceLocs(result))
         })
