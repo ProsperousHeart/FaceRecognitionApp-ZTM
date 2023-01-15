6ee76fd642ce605571d5760ee3fd0791ddf2922c
Kassandra Keeton (the Prosperous Heart) <ProsperousHeart@users.noreply.github.com>
2023-01-14 21:23:43 -0600

    migration of Clarifai API to backend

--- facerecognitionapp-ztm/src/App.js
+++ facerecognitionapp-ztm/src/App.js
@@ -53,7 +53,8 @@ class App extends Component {
 
   calcFaceLocs = (data) => {
     //const clarifaiFaces = data.outputs[0].data.regions[0].region_info.bounding_box;
-    const clarifaiFaces = data.outputs[0].data.regions;
+    // const clarifaiFaces = data.outputs[0].data.regions;
+    const clarifaiFaces = data;
     const image = document.getElementById('intputIMG');
     const width = Number(image.width);
     const height = Number(image.height);
@@ -92,82 +93,40 @@ class App extends Component {
     this.setState({ imgURL: this.state.input }, 
       () => console.log("SETTING URL:", this.state.imgURL));
     
-    //////////////////////////////////////////////////////////////////////////////////////////
-    // In this section, we set the user authentication, app ID, model details, and the URL
-    // of the image we want as an input. Change these strings to run your own example.
-    /////////////////////////////////////////////////////////////////////////////////////////
-
-    const USER_ID = 'prosperousheart';
-    // Your PAT (Personal Access Token) can be found in the portal under Authentification
-    const PAT = '7773bac5e6d24f0cac58ad719eba396f';
-    const APP_ID = 'face-recog-app';
-    // Change these to whatever model and image URL you want to use
-    const MODEL_ID = 'face-detection';
-    //const MODEL_VERSION_ID = 'aa7f35c01e0642fda5cf400f543e7c40';    
-    //const IMAGE_URL = this.state.imgURL;
-    const IMAGE_URL = this.state.input;
-
-    const raw = JSON.stringify({
-      "user_app_id": {
-        //"user_id": "clarifai",
-        "user_id": USER_ID,
-        //"app_id": "main"
-        "app_id": APP_ID
-      },
-      "inputs": [
-          {
-              "data": {
-                  "image": {
-                      "url": IMAGE_URL
-                  }
-              }
-          }
-      ]
-    });
-    //console.log(raw);
-
-    const requestOptions = {
-        method: 'POST',
-        headers: {
-            'Accept': 'application/json',
-            'Authorization': 'Key ' + PAT // https://docs.clarifai.com/clarifai-basics/authentication/personal-access-tokens/
-        },
-        body: raw
-    };
-
-    // NOTE: MODEL_VERSION_ID is optional, you can also call prediction with the MODEL_ID only
-    // https://api.clarifai.com/v2/models/{YOUR_MODEL_ID}/outputs
-    // this will default to the latest version_id
-
-    // https://clarifai.com/clarifai/main/models/face-detection
-    //fetch("https://api.clarifai.com/v2/models/" + MODEL_ID + "/versions/" + MODEL_VERSION_ID + "/outputs", requestOptions)
-    fetch("https://api.clarifai.com/v2/models/" + MODEL_ID + "/outputs", requestOptions)
-        //.then(response => response.text()) // originally provided
-        .then(response => response.json())
-        //.then(result => console.log(result)) // originally provided
-        .then(result => {
-          if (result) {
-            fetch('http://localhost:3000/image', {
-              method: 'put',
-              headers: {'Content-Type': 'application/json'},
-              body: JSON.stringify({
-                  id: this.state.user.id
-              })
-            })
-              .then(res => res.json())
-              .then(count => {
-                /*this.setState({
-                  user: {
-                    entries: count
-                  }
-                })*/
-                this.setState(Object.assign(this.state.user, { entries: count}));
-              })
-              .catch(console.log)
-          }
-          this.dispFaceBox(this.calcFaceLocs(result))
+      fetch('http://localhost:3000/imageURL', {
+        method: 'post',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({
+            input: this.state.input
         })
-        .catch(error => console.log('error', error));  // promise if something fails
+      })
+      .then(response => response.json())
+      //.then(result => console.log(result)) // originally provided
+      .then(result => {
+        // console.log("onIMGSubmit API call:", result);
+        if (result) {
+          this.dispFaceBox(result)
+          fetch('http://localhost:3000/image', {
+            method: 'put',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                id: this.state.user.id
+            })
+          })
+            .then(res => res.json())
+            .then(count => {
+              /*this.setState({
+                user: {
+                  entries: count
+                }
+              })*/
+              this.setState(Object.assign(this.state.user, { entries: count}));
+            })
+            .catch(console.log)
+        }
+        this.dispFaceBox(this.calcFaceLocs(result))
+      })
+      .catch(error => console.log('error', error));  // promise if something fails
   }
 
   onRouteChg = (route) => {
